65164c8dc1b89ccd1b5c0fb23c7e21543874a8e7
vaibhavthapliyal-moglix <vaibhav.thapliyal@moglix.com>
2019-10-16 16:26:42 +0530

    Added Implementation to add node at the beginning and at any nth position

--- src/main/java/com/java/datastructure/impl/LinkedList.java
+++ src/main/java/com/java/datastructure/impl/LinkedList.java
@@ -1,22 +1,26 @@
 package com.java.datastructure.impl;
 
 public class LinkedList<T> {
-	
+
 	private Node head;
-	
+
 	public class Node{
 		T data;
 		Node next;
-		
+
 		public Node(T data) {
 			this.data = data;
 			this.next = null;
 		}
 	}
-	
+
+	/**
+	 * Add node to a linkedlist
+	 * @param data
+	 */
 	public void add(T data) {
 		Node node = new Node(data);
-		
+
 
 		if(head == null) {
 			head = node;
@@ -29,6 +33,37 @@ public class LinkedList<T> {
 		}
 	}
 
+	/**
+	 * Add Node at the beginning of a linkedList
+	 * @param data
+	 */
+	public void addInBeginning(T data){
+		Node node = new Node(data);
+		node.next = head;
+		head = node;
+	}
+
+	/**
+	 * Add node at the nth index of a linked list
+	 * @param index
+	 * @param data
+	 */
+	public void add(int index, T data){
+		Node node = new Node(data);
+		Node curreNode = this.getHead();
+		for (int i = 1; i < index; i++){
+			curreNode = curreNode.next;
+		}
+		if(curreNode != null){
+			Node nextNode = curreNode.next;
+			curreNode.next = node;
+			if(nextNode != null){
+				node.next = nextNode;
+			}
+		}
+	}
+
+
 	public Node getHead() {
 		return head;
 	}
@@ -36,8 +71,8 @@ public class LinkedList<T> {
 	public void setHead(Node head) {
 		this.head = head;
 	}
-	
-	
+
+
 	@Override
 	public String toString() {
 		String list = "";
@@ -46,21 +81,21 @@ public class LinkedList<T> {
 			list = list + "," + currNode.data;
 			currNode = currNode.next;
 		}
-		
+
 		return "[" + list.substring(1, list.length()) + "]";
 	}
 
 	public static void main(String[] args) {
-		
+
 		LinkedList<Integer> data = new LinkedList<>();
-		
-		data.add(5);
-		
-		data.add(2);
-		
 		data.add(1);
-		
+		data.add(2);
+		data.add(9);
+		data.add(2, 7);
+		data.add(10);
+		data.add(4,6);
+
 		System.out.println(data);
 	}
-	
+
 }
